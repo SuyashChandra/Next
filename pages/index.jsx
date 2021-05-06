3ed24ca08a7fcfdc5cfef0e06d434c8592af8ee8
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-05-06 11:48:40 +0530

    Connecting theme-ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Link from "next/link";
-import styles from "../src/styles.module.css";
 
 const Page = () => (
   <div>
-    <h1 className={styles.note}>Index Page</h1>
+    <h1>Index Page</h1>
     <Link href="/notes">
       <a>Note</a>
     </Link>
