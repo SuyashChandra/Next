442a10b1e8815aa912f4ea0425fbe5bf078ff43d
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-05-06 09:37:14 +0530

    Page Navigation

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -4,9 +4,9 @@ import { useRouter } from "next/router";
 const Page = () => {
   const router = useRouter();
 
-  const { params } = router.query;
-  console.log(params);
-  return <h1>Note {params}</h1>;
+  const { id } = router.query;
+  // console.log(params);
+  return <h1>Note {id}</h1>;
 };
 
 export default Page;
