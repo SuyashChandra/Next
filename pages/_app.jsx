b0964f6990d9e0ff793a00918bc931ab245fdb95
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-05-07 13:04:27 +0530

    config for environment variables

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+/** @jsx jsx */
+/** @jsxRuntime classic */
+import { jsx } from "theme-ui";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
+import Nav from "../src/components/nav";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />;
+      <div>
+        <Nav />
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 }
--- theme.js
+++ theme.js
@@ -2,10 +2,7 @@ import { roboto } from "@theme-ui/presets";
 
 const theme = {
   ...roboto,
-  colors: {
-    ...roboto.colors,
-    background: "yellow",
-  },
+
   containers: {
     card: {
       boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
